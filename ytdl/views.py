3b0fba4facd0c4b50f7fc0a1d06c4aa82afa2183
carlagesa <carltonmbaja0@gmail.com>
2023-08-03 03:15:46 -0700

    Optimized code using asyn

--- ytdl/views.py
+++ ytdl/views.py
@@ -1,63 +1,56 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
-import youtube_dl
 from .forms import DownloadForm
+import youtube_dl
 import re
+import asyncio
 
+async def extract_video_info(video_url):
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        return await asyncio.to_thread(ydl.extract_info, video_url, download=False)
 
-def download_video(request):
-    global context
+async def download_video(request):
     form = DownloadForm(request.POST or None)
     
     if form.is_valid():
         video_url = form.cleaned_data.get("url")
         regex = r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'
-        #regex = (r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$\n")
-        print(video_url)
-        if not re.match(regex,video_url):
-            print('nhi hoa')
-            return HttpResponse('Enter correct url.')
+        if not re.match(regex, video_url):
+            return HttpResponse('Enter correct URL.')
 
-        # if 'm.' in video_url:
-        #     video_url = video_url.replace(u'm.', u'')
-
-        # elif 'youtu.be' in video_url:
-        #     video_id = video_url.split('/')[-1]
-        #     video_url = 'https://www.youtube.com/watch?v=' + video_id
-
-        # if len(video_url.split("=")[-1]) < 11:
-        #     return HttpResponse('Enter correct url.')
-
-        ydl_opts = {}
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            meta = ydl.extract_info(
-                video_url, download=False)
+        meta = await extract_video_info(video_url)
+        
         video_audio_streams = []
         for m in meta['formats']:
-            file_size = m['filesize']
+            file_size = m.get('filesize')
             if file_size is not None:
-                file_size = f'{round(int(file_size) / 1000000,2)} mb'
+                file_size = f'{round(int(file_size) / 1000000, 2)} mb'
 
             resolution = 'Audio'
-            if m['height'] is not None:
+            if m.get('height') is not None:
                 resolution = f"{m['height']}x{m['width']}"
+
             video_audio_streams.append({
                 'resolution': resolution,
-                'extension': m['ext'],
+                'extension': m.get('ext'),
                 'file_size': file_size,
-                'video_url': m['url']
+                'video_url': m.get('url')
             })
+
         video_audio_streams = video_audio_streams[::-1]
+
         context = {
             'form': form,
-            'title': meta['title'], 'streams': video_audio_streams,
-            'description': meta['description'], 'likes': meta['like_count'],
-            'dislikes': meta['dislike_count'], 'thumb': meta['thumbnails'][3]['url'],
-            'duration': round(int(meta['duration'])/60, 2), 'views': f'{int(meta["view_count"]):,}'
+            'title': meta.get('title'),
+            'streams': video_audio_streams,
+            'description': meta.get('description'),
+            'likes': meta.get('like_count'),
+            'dislikes': meta.get('dislike_count'),
+            'thumb': meta.get('thumbnails', [{}])[3].get('url'),
+            'duration': round(int(meta.get('duration', 0)) / 60, 2),
+            'views': f'{int(meta.get("view_count", 0)):,}'
         }
         return render(request, 'home.html', context)
+
     return render(request, 'home.html', {'form': form})
