8fc41b13d3d82a66a2c19be414689e91982fcf37
carlagesa <carltonmbaja0@gmail.com>
2023-08-03 03:48:51 -0700

    Changed "asyncio" to "ThreadPoolExecutor"

--- ytdl/views.py
+++ ytdl/views.py
@@ -1,16 +1,74 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from .forms import DownloadForm
+# import youtube_dl
+# import re
+# import asyncio
+
+# async def extract_video_info(video_url):
+#     ydl_opts = {}
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         return await asyncio.to_thread(ydl.extract_info, video_url, download=False)
+
+# async def download_video(request):
+#     form = DownloadForm(request.POST or None)
+    
+#     if form.is_valid():
+#         video_url = form.cleaned_data.get("url")
+#         regex = r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'
+#         if not re.match(regex, video_url):
+#             return HttpResponse('Enter correct URL.')
+
+#         meta = await extract_video_info(video_url)
+        
+#         video_audio_streams = []
+#         for m in meta['formats']:
+#             file_size = m.get('filesize')
+#             if file_size is not None:
+#                 file_size = f'{round(int(file_size) / 1000000, 2)} mb'
+
+#             resolution = 'Audio'
+#             if m.get('height') is not None:
+#                 resolution = f"{m['height']}x{m['width']}"
+
+#             video_audio_streams.append({
+#                 'resolution': resolution,
+#                 'extension': m.get('ext'),
+#                 'file_size': file_size,
+#                 'video_url': m.get('url')
+#             })
+
+#         video_audio_streams = video_audio_streams[::-1]
+
+#         context = {
+#             'form': form,
+#             'title': meta.get('title'),
+#             'streams': video_audio_streams,
+#             'description': meta.get('description'),
+#             'likes': meta.get('like_count'),
+#             'dislikes': meta.get('dislike_count'),
+#             'thumb': meta.get('thumbnails', [{}])[3].get('url'),
+#             'duration': round(int(meta.get('duration', 0)) / 60, 2),
+#             'views': f'{int(meta.get("view_count", 0)):,}'
+#         }
+#         return render(request, 'home.html', context)
+
+#     return render(request, 'home.html', {'form': form})
+
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import DownloadForm
 import youtube_dl
 import re
-import asyncio
+import concurrent.futures
 
-async def extract_video_info(video_url):
+def extract_video_info(video_url):
     ydl_opts = {}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        return await asyncio.to_thread(ydl.extract_info, video_url, download=False)
+        return ydl.extract_info(video_url, download=False)
 
-async def download_video(request):
+def download_video(request):
     form = DownloadForm(request.POST or None)
     
     if form.is_valid():
@@ -19,7 +77,8 @@ async def download_video(request):
         if not re.match(regex, video_url):
             return HttpResponse('Enter correct URL.')
 
-        meta = await extract_video_info(video_url)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            meta = executor.submit(extract_video_info, video_url).result()
         
         video_audio_streams = []
         for m in meta['formats']:
